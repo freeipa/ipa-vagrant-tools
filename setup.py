93a84125d6d1aa0e3eb256207e9903ab965cdad3
Martin Basti <mbasti@redhat.com>
2016-01-17 21:15:16 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# Author: Martin Basti
+# See LICENSE file for license
+
+from distutils.core import setup
+
+setup(name='ipa-vagrant-devel-tools',
+      version='0.3',
+      description='Useful scripts for creating test and delopment environment for IPA',
+      author='Martin Basti',
+      author_email='martin.basti@gmail.com',
+      url='https://github.com/bastiak/ipa-devel-tools',
+      packages=['ipavagrant'],
+     )
