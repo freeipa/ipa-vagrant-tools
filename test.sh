9b846b3cf2c4b35f8c15fccca4d32f7fa527cf16
Martin <martin.basti@gmail.com>
2016-02-27 21:31:36 +0100

    CI: remove tests.sh from CI testing

--- test.sh
+++ test.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env sh
-
 # run all tests
 
 # pylint does not like pytest (tests/ dir excluded from check)
 pylint ipavagrant ipa-vagrant-ci-*
-PYLINT_RES=$?
 
 pep8 ipavagrant/* ipa-vagrant-ci-* tests/*
-PEP8_RES=$?
 
 py.test tests/ --verbose
-PYTEST_RES=$?
-
-exit $PYLINT_RES || $PEP8_RES || $PYTEST_RES
