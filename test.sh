0146327596962c42e0f3cccec8bd44a837f0f04c
Martin <martin.basti@gmail.com>
2016-02-27 13:45:39 +0100

    CI: fix test script

--- test.sh
+++ test.sh
@@ -4,14 +4,8 @@
 
 pylint ipavagrant ipa-vagrant-ci-*
 PYLINT_RES=$?
-if [[ $PYLINT_RES = 0 ]]; then
-    echo "pylint: PASS"
-fi
 
 pep8 ipavagrant/* ipa-vagrant-ci-*
 PEP8_RES=$?
-if [[ $PEP8_RES = 0 ]]; then
-    echo "PEP8: PASS"
-fi
 
 exit $PYLINT_RES || $PEP8_RES
