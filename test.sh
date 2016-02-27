a61e0b3258298d97755138dd0a39d344e949857c
Martin <martin.basti@gmail.com>
2016-02-27 13:33:53 +0100

    fix: run both pep8 and pylint

---
+++ test.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+# run all tests
+
+pylint ipavagrant ipa-vagrant-ci-*
+PYLINT_RES=$?
+if [[ $PYLINT_RES = 0 ]]; then
+    echo "pylint: PASS"
+fi
+
+pep8 ipavagrant/* ipa-vagrant-ci-*
+PEP8_RES=$?
+if [[ $PEP8_RES = 0 ]]; then
+    echo "PEP8: PASS"
+fi
+
+exit $PYLINT_RES || $PEP8_RES
